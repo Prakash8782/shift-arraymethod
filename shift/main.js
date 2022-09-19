71f5dc7bc150eb9444235480a78b71bfbf1039e4
Prakash8782 <prakash8782@gmail.com>
2022-09-19 15:50:49 +0530

    shift

---
+++ shift/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : ", colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.shift()");
+colors.shift("Red");
+console.log("change in array :", colors);
+console.log("return : length of array after shift(); is "+colors.length);
